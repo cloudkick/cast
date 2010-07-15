cc18aa0388825896cd95ddbc6e6a3f12166e93c6
Paul Querna <pquerna@apache.org>
2010-07-14 17:39:25 -0700

    Start on the http bundle storage service, creating a new test case along the way

--- tests/foo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'test String#length': function(assert){
-        assert.equal(6, 'foobar'.length);
-        assert.equal(6, 'foobar');
-    }
-}
-
---
+++ tests/http_bundles.js
@@ -0,0 +1,63 @@
+/*
+ * Licensed to Cloudkick, Inc ('Cloudkick') under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * Cloudkick licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var ps = require('util/pubsub');
+var sys = require('sys');
+
+function getServer()
+{
+  return require('services/http')._serverOnly();
+}
+
+var hello = "Hello World";
+
+exports['PUT t.txt'] = function(assert, beforeExit) {
+  var n = 0;
+
+  assert.response(getServer(), {
+    url: '/bundles/foo/t.txt',
+    method: 'PUT',
+    data: hello,
+  },
+  function(res) {
+    assert.equal(204, res.statusCode);
+    assert.length(0, res.body);
+    n++;
+    ps.emit('t.txt created');
+  });
+  beforeExit(function(){
+    assert.equal(1, n, 'Responses Received');
+  });
+};
+
+exports['GET t.txt'] = function(assert, beforeExit) {
+  var n = 0;
+  ps.on('t.txt created', function() {
+    assert.response(getServer(), {
+      url: '/bundles/foo/t.txt',
+      method: 'GET'
+    },
+    function(res) {
+      assert.equal(200, res.statusCode);
+      assert.equal(hello, res.body)
+      n++;
+    });
+  });
+  beforeExit(function(){
+    assert.equal(1, n, 'Responses Received');
+  });
+};
