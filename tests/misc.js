18f28c77d55ffb3b66ad3849df27045c070dc4d0
Paul Querna <pquerna@apache.org>
2010-07-23 00:22:36 -0700

    Add misc.expanduser

--- tests/misc.js
+++ tests/misc.js
@@ -37,3 +37,60 @@ exports['empty merge'] = function(assert, beforeExit) {
   var out = misc.merge(a, b);
   assert.deepEqual(c, out);
 };
+
+exports['empty expanduser'] = function(assert, beforeExit) {
+  var out = misc.expanduser("/foo/bar");
+  assert.equal("/foo/bar", out);
+};
+
+exports['expanduser on self'] = function(assert, beforeExit) {
+  var out = misc.expanduser("~/foo/bar");
+  assert.equal(process.env.HOME+"/foo/bar", out);
+};
+
+exports['expanduser on nothing'] = function(assert, beforeExit) {
+  var path = require('path');
+  var out = misc.expanduser("~");
+  assert.equal(path.join(process.env.HOME, "/"), out);
+};
+
+exports['expanduser with HOME unset hack'] = function(assert, beforeExit) {
+  beforeExit(function() {
+    var n = 0;
+    var orig = process.env.HOME;
+    process.env.HOME = undefined;
+    try {
+      var out = misc.expanduser("~");
+    }
+    catch (e1) {
+      n++;
+      assert.match(e1, /was undefined/);
+    }
+    process.env.HOME = orig;
+    assert.equal(1, n, 'Exceptions thrown');
+  })
+};
+
+
+exports['missing getpwnam for expanduser'] = function(assert, beforeExit) {
+  var n = 0;
+  try {
+    var out = misc.expanduser("~root/foo/bar");
+  }
+  catch (e1) {
+    n++;
+    assert.match(e1, /getpwnam/);
+  }
+
+  try {
+    var out = misc.expanduser("~root");
+  }
+  catch (e2) {
+    n++;
+    assert.match(e2, /getpwnam/);
+  }
+
+  beforeExit(function(){
+    assert.equal(2, n, 'Exceptions thrown');
+  });
+};
