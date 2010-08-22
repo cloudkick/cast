4cf1a35484fc96bcc1caef38bb8ea4a73e912dae
Russell Haering <russell@cloudkick.com>
2010-08-22 04:23:42 -0700

    More extensive service management tests. Currently failing due to a legitimate bug.

--- tests/service_management.js
+++ tests/service_management.js
@@ -17,35 +17,19 @@
 
 var sys = require('sys');
 var fs = require('fs');
+var path = require('path');
 var exec = require('child_process').exec;
 var ps = require('util/pubsub');
 var misc = require('util/misc');
 var async = require('extern/async');
-var runit = require('lib/runit');
-
-var completed = 0;
-var total = 0;
-
-// Is there a way to do this automagically?
-function has_paths(count) {
-  total += count;
-}
-
-function finish_path() {
-  if (--total === 0) {
-    ps.emit(ps.AGENT_STATE_STOP);
-  }
-}
+var runit = require('runit/services');
 
 function getServer()
 {
   return require('services/http')._serverOnly();
 }
 
-
-
 // For now these need to all be in the same test, this blows
-has_paths(1);
 exports["service management"] = function(assert, beforeExit) {
   var n = 0;
   var tasks = [];
@@ -53,6 +37,49 @@ exports["service management"] = function(assert, beforeExit) {
 
   // Add a bunch of tasks
   // These all happen serially in the order they are added
+
+  // Make sure the list call works and returns an empty array
+  tasks.push(function(callback) {
+    assert.response(getServer(), {
+      url: '/services/',
+      method: 'GET'
+    },
+    function(res) {
+      n++;
+      try {
+        assert.equal(res.statusCode, 200);
+        var data = JSON.parse(res.body);
+        assert.ok(data instanceof Array);
+        assert.equal(data.length, 0);
+      }
+      catch (err) {
+        return callback(err);
+      }
+      return callback();
+    });
+  });
+
+  // Create a basic services layout
+  tasks.push(function(callback) {
+    svcdir.create_service_layout('foo', function(err) {
+      n++;
+      if (err) {
+        return callback(err);
+      }
+      return callback();
+    });
+  });
+
+  tasks.push(function(callback) {
+    var fakerun_path = path.join(__dirname, 'fake_service', 'run');
+    exec('cp ' + fakerun_path + ' .tests/services/available/foo/run', function(err) {
+      n++;
+      if (err) {
+        return callback(err);
+      }
+      return callback();
+    });
+  });
 
   tasks.push(function(callback) {
     assert.response(getServer(), {
@@ -64,33 +91,158 @@ exports["service management"] = function(assert, beforeExit) {
       try {
         assert.equal(res.statusCode, 200);
         var data = JSON.parse(res.body);
-        assert.equal(typeof(data), 'array');
-        assert.equal(data.length, 0);
+        assert.ok(data instanceof Array);
+        assert.equal(data.length, 1);
       }
-      finally {
-        finish_path();
-        callback();
+      catch (err) {
+        return callback(err);
       }
+      return callback();
     });
   });
 
   tasks.push(function(callback) {
-    svcdir.create_service_layout('foo', function() {
-      
-
+    assert.response(getServer(), {
+      url: '/services/foo/',
+      method: 'GET'
+    },
+    function(res) {
+      n++;
+      try {
+        assert.equal(res.statusCode, 200);
+        var data = JSON.parse(res.body);
+        assert.ok(data instanceof Object);
+        assert.equal(data.name, "foo");
+        assert.equal(data.enabled, false);
+        assert.equal(data.normally, "down");
+      }
+      catch (err) {
+        return callback(err);
+      }
+      return callback();
     });
-    
-
   });
 
+  tasks.push(function(callback) {
+    assert.response(getServer(), {
+      url: '/services/foo/restart/',
+      method: 'PUT'
+    },
+    function(res) {
+      n++;
+      try {
+        assert.equal(res.statusCode, 500);
+        var data = JSON.parse(res.body);
+        assert.ok(data instanceof Object);
+        assert.equal(data.code, 500);
+        assert.ok(data.message);
+      }
+      catch (err) {
+        return callback(err);
+      }
+      return callback();
+    });
+  });
 
-  async.series(tasks, function() {
-    n++;
+  tasks.push(function(callback) {
+    assert.response(getServer(), {
+      url: '/services/foo/enable/',
+      method: 'PUT'
+    },
+    function(res) {
+      n++;
+      try {
+        assert.equal(res.statusCode, 200);
+      }
+      catch (err) {
+        callback(err);
+      }
+      return callback();
+    });
+  });
+
+  tasks.push(function(callback) {
+    assert.response(getServer(), {
+      url: '/services/foo/',
+      method: 'GET'
+    },
+    function(res) {
+      n++;
+      try {
+        assert.equal(res.statusCode, 200);
+        var data = JSON.parse(res.body);
+        assert.ok(data instanceof Object);
+        assert.equal(data.name, "foo");
+        assert.equal(data.enabled, true);
+        assert.equal(data.normally, "down");
+      }
+      catch (err) {
+        return callback(err);
+      }
+      return callback();
+    });
+  });
+
+  // TODO: Actually check that these are happening
+  tasks.push(function(callback) {
+    assert.response(getServier(), {
+      url: '/services/foo/stop/',
+      method: 'PUT'
+    },
+    function(res) {
+      n++;
+      try {
+        assert.equal(res.statusCode, 200);
+      }
+      catch (err) {
+        callback(err);
+      }
+      return callback();
+    });
+  });
+
+  tasks.push(function(callback) {
+    assert.response(getServier(), {
+      url: '/services/foo/start/',
+      method: 'PUT'
+    },
+    function(res) {
+      n++;
+      try {
+        assert.equal(res.statusCode, 200);
+      }
+      catch (err) {
+        callback(err);
+      }
+      return callback();
+    });
+  });
+
+  tasks.push(function(callback) {
+    assert.response(getServier(), {
+      url: '/services/foo/restart/',
+      method: 'PUT'
+    },
+    function(res) {
+      n++;
+      try {
+        assert.equal(res.statusCode, 200);
+      }
+      catch (err) {
+        callback(err);
+      }
+      return callback();
+    });
   });
 
   // Execute the tasks
+  async.series(tasks, function(err) {
+    ps.emit(ps.AGENT_STATE_STOP);
+    assert.ifError(err);
+  });
+
   beforeExit(function(){
-    assert.equal(2, n, 'Tests Completed');
+    assert.equal(6, n, 'Tests Completed');
   });
 };
 
@@ -98,7 +250,8 @@ exports.setup = function(done) {
   async.series([
     async.apply(ps.ensure, "config"),
     async.apply(exec, "mkdir .tests/services"),
-    async.apply(exec, "mkdir .tests/services-available"),
+    async.apply(exec, "mkdir .tests/services/available"),
+    async.apply(exec, "mkdir .tests/services/enabled"),
     function(callback) {
       require('services/runit').load();
       ps.emit(ps.AGENT_STATE_START);
