4b2ff4b592fa9910cf02773b1f9c82712e9d5e5e
Russell Haering <russell@cloudkick.com>
2011-02-07 01:30:26 -0800

    Add norris tests

---
+++ tests/simple/norris.js
@@ -0,0 +1,41 @@
+/*
+ * Licensed to Cloudkick, Inc ('Cloudkick') under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * Cloudkick licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var norris = require('norris');
+var async = require('extern/async');
+var assert = require('assert');
+
+(function() {
+  var completed = false;
+  norris.get(function(facts)  {
+    // Check the hostname
+    assert.ok(facts.hostname);
+
+    // Check the architecture
+    assert.ok(facts.arch);
+    assert.ok(facts.arch == "i386" || facts.arch == "x86_64");
+
+    // Check gnutar
+    assert.ok(facts.gnutar);
+
+    completed = true;
+  });
+
+  process.on('exit', function() {
+    assert.ok(completed, 'Tests completed');
+  });
+})();
