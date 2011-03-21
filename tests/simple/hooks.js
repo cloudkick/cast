9700180cc6f7a5a07e81ad3fbe806ad152477d18
Tomaz Muraus <tomaz@cloudkick.com>
2011-03-21 20:39:02 +0100

    Base Hook class should behave the same as the InstanceHook class if it gets passed the correct arguments.

--- tests/simple/hooks.js
+++ tests/simple/hooks.js
@@ -26,10 +26,11 @@ instance._bundleName = 'test_bundle';
 instance.root = path.join(__dirname, '../data/instances/test_instance/');
 
 var instance_version_path = path.join(instance.root, 'versions/test_bundle@1.0');
+var hooks_path = path.join(instance_version_path, '.cast-project/hooks');
 
 // Test success (exit code == 0)
 (function() {
-  var completed = false;
+  var n = 0;
 
   var callback = function(err, killed, stdout, stderr) {
     assert.ifError(err);
@@ -38,21 +39,26 @@ var instance_version_path = path.join(instance.root, 'versions/test_bundle@1.0')
     assert.equal('test hook success stdout', stdout);
     assert.equal('test hook success stderr', stderr);
 
-    completed = true;
+    n++;
   };
 
-  var hook = new hooks.InstanceHook('pre', 'hook_success.js',
-                                    instance_version_path);
-  hook.execute(null, [], callback);
+  var hook_file = 'hook_success.js';
+  var base_hook = new hooks.Hook('pre', hook_file,
+                                  path.join(hooks_path, hook_file),
+                                  false);
+  var instance_hook = new hooks.InstanceHook('pre', hook_file,
+                                             instance_version_path);
+  base_hook.execute(null, [], callback);
+  instance_hook.execute(null, [], callback);
 
   process.on('exit', function() {
-    assert.ok(completed, 'Tests completed');
+    assert.equal(n, 2, 'Tests completed');
   });
 })();
 
 // Test failure (exit code != 0)
 (function() {
-  var completed = false;
+  var n = 0;
 
   var callback = function(err, killed, stdout, stderr) {
     assert.ok(err);
@@ -61,21 +67,26 @@ var instance_version_path = path.join(instance.root, 'versions/test_bundle@1.0')
     assert.equal('test hook failure stdout', stdout);
     assert.equal('test hook failure stderr', stderr);
 
-    completed = true;
+    n++;
   };
 
-  var hook = new hooks.InstanceHook('pre', 'hook_failure.js',
-                                    instance_version_path);
-  hook.execute(null, [], callback);
+  var hook_file = 'hook_failure.js';
+  var base_hook = new hooks.Hook('pre', hook_file,
+                                  path.join(hooks_path, hook_file),
+                                  false);
+  var instance_hook = new hooks.InstanceHook('pre', hook_file,
+                                             instance_version_path);
+  base_hook.execute(null, [], callback);
+  instance_hook.execute(null, [], callback);
 
   process.on('exit', function() {
-    assert.ok(completed, 'Tests completed');
+    assert.equal(n, 2, 'Tests completed');
   });
 })();
 
 // Test hook args
 (function() {
-  var completed = false;
+  var n = 0;
 
   var callback = function(err, killed, stdout, stderr) {
     assert.ifError(err);
@@ -84,57 +95,72 @@ var instance_version_path = path.join(instance.root, 'versions/test_bundle@1.0')
     assert.equal('test hook args stdout: test1, test2', stdout);
     assert.equal('test hook args stderr', stderr);
 
-    completed = true;
+    n++;
   };
 
-  var hook = new hooks.InstanceHook('pre', 'hook_args.js',
-                                    instance_version_path);
-  hook.execute(null, [ 'test1', 'test2'], callback);
+  var hook_file = 'hook_args.js';
+  var base_hook = new hooks.Hook('pre', hook_file,
+                                  path.join(hooks_path, hook_file),
+                                  false);
+  var instance_hook = new hooks.InstanceHook('pre', hook_file,
+                                             instance_version_path);
+  base_hook.execute(null, [ 'test1', 'test2'], callback);
+  instance_hook.execute(null, [ 'test1', 'test2'], callback);
 
   process.on('exit', function() {
-    assert.ok(completed, 'Tests completed');
+    assert.equal(n, 2, 'Tests completed');
   });
 })();
 
 // Test timeout
 (function() {
-  var completed = false;
+  var n = 0;
 
   var callback = function(err, killed, stdout, stderr) {
     assert.ok(err);
     assert.match(err, /timeout/);
     assert.ok(killed);
 
-    completed = true;
+    n++;
   };
 
-  var hook = new hooks.InstanceHook('pre', 'hook_timeout.js',
-                                    instance_version_path);
-  hook.execute(300, [], callback);
+  var hook_file = 'hook_timeout.js';
+  var base_hook = new hooks.Hook('pre', hook_file,
+                                  path.join(hooks_path, hook_file),
+                                  false);
+  var instance_hook = new hooks.InstanceHook('pre', hook_file,
+                                             instance_version_path);
+  base_hook.execute(300, [], callback);
+  instance_hook.execute(300, [], callback);
 
   process.on('exit', function() {
-    assert.ok(completed, 'Tests completed');
+    assert.equal(n, 2, 'Tests completed');
   });
 })();
 
 // Test failure (hook script is not executable)
 (function() {
-  var completed = false;
+  var n = 0;
 
   var callback = function(err, killed, stdout, stderr) {
     assert.ok(err);
     assert.match(err, /status 127/);
     assert.ok(!killed);
 
-    completed = true;
+    n++;
   };
 
-  var hook = new hooks.InstanceHook('pre', 'hook_not_executable.js',
-                                    instance_version_path);
-  hook.execute(null, [], callback);
+  var hook_file = 'hook_not_executable.js';
+  var base_hook = new hooks.Hook('pre', hook_file,
+                                  path.join(hooks_path, hook_file),
+                                  false);
+  var instance_hook = new hooks.InstanceHook('pre', hook_file,
+                                             instance_version_path);
+  base_hook.execute(null, [], callback);
+  instance_hook.execute(null, [], callback);
 
   process.on('exit', function() {
-    assert.ok(completed, 'Tests completed');
+    assert.equal(n, 2, 'Tests completed');
   });
 })();
 
@@ -157,14 +183,15 @@ var instance_version_path = path.join(instance.root, 'versions/test_bundle@1.0')
     n++;
   };
 
-  var hook1 = new hooks.InstanceHook('pre', 'hook_not_exists.js',
+  var hook_file = 'hook_not_exists.js';
+  var hook1 = new hooks.InstanceHook('pre', hook_file,
                                      instance_version_path, false);
-  var hook2 = new hooks.InstanceHook('pre', 'hook_not_exists.js',
+  var hook2 = new hooks.InstanceHook('pre', hook_file,
                                      instance_version_path, true);
   hook1.execute(null, [], callback1);
   hook2.execute(null, [], callback2);
 
   process.on('exit', function() {
-    assert.equal(n, 2);
+    assert.equal(n, 2, 'Tests completed');
   });
 })();
