f3fa976a2aca381df7bf7b5a1377592070f1fd37
Tomaz Muraus <tomaz@cloudkick.com>
2011-03-27 14:24:14 +0200

    Port util-fs-mkdir.js tests.

--- tests/simple/util-fs-mkdir.js
+++ tests/simple/util-fs-mkdir.js
@@ -21,102 +21,117 @@ var fsutil = require('util/fs');
 var async = require('extern/async');
 var assert = require('assert');
 
-(function() {
-  var PATH1 = '.tests/utilfs/foo';
-  var PATH1_SUBDIR = '.tests/utilfs/foo/bar';
-  var PATH2 = '.tests/utilfs/bar/baz';
-  var PATH3 = '.tests/utilfs/baz/buz';
-  var PATH4 = '.tests/utilfs/whammy';
-  var completed = false;
+var PATH1 = '.tests/utilfs/foo';
+var PATH1_SUBDIR = '.tests/utilfs/foo/bar';
+var PATH2 = '.tests/utilfs/bar/baz';
+var PATH3 = '.tests/utilfs/baz/buz';
+var PATH4 = '.tests/utilfs/whammy';
 
+exports['test_mkdir_file_exists'] = function() {
   async.series([
-    async.apply(fs.mkdir, '.tests/utilfs', 0700),
-
-    // Create a file
-    async.apply(fs.writeFile, PATH1, 'test'),
-
-    // Attempt to create a directory at the same path
-    function(callback) {
-      fsutil.mkdir(PATH1, function(err) {
-        assert.ok(err);
-        assert.match(err.message, /File exists/);
-        callback();
-      });
-    },
-
-    // Make sure the file still exists
-    function(callback) {
-      fs.stat(PATH1, function(err, stats) {
-        assert.ifError(err);
-        assert.ok(!stats.isDirectory());
-        callback();
-      });
-    },
-
-    // Attempt to create a directory within the file
-    function(callback) {
-      fsutil.mkdir(PATH1_SUBDIR, function(err) {
-        assert.ok(err);
-        assert.match(err.message, /File exists/);
-        callback();
-      });
-    },
-
-    // Make sure the file still exists
-    function(callback) {
-      fs.stat(PATH1, function(err, stats) {
-        assert.ifError(err);
-        assert.ok(!stats.isDirectory());
-        callback();
-      });
-    },
-
-    // Recursive mkdir at a relative path
-    async.apply(fsutil.mkdir, PATH2, 0700),
-
-    // Make sure it worked
-    function(callback) {
-      fs.stat(PATH2, function(err, stats) {
-        assert.ifError(err);
-        assert.ok(stats.isDirectory());
-        assert.equal(stats.mode & 0777, 0700);
-        callback();
-      });
-    },
-
-    // Recursive mkdir at an absolute path
-    async.apply(fsutil.mkdir, path.join(process.cwd(), PATH3), 0700),
-
-    // Make sure it worked
-    function(callback) {
-      fs.stat(PATH3, function(err, stats) {
-        assert.ifError(err);
-        assert.ok(stats.isDirectory());
-        assert.equal(stats.mode & 0777, 0700);
-        callback();
-      });
-    },
-
-    // Mkdir with implicit perms
-    async.apply(fsutil.mkdir, PATH4),
-
-    // Make sure it worked
-    function(callback) {
-      fs.stat(PATH4, function(err, stats) {
-        assert.ifError(err);
-        assert.ok(stats.isDirectory());
-        //assert.equal(stats.mode & 0777, 0755);
-        callback();
-      });
+  function(callback) {
+    fs.mkdir, '.tests/utilfs', 0700, function(err) {
+      callback();
     }
-  ],
+  },
+
+  // Create a file
+  async.apply(fs.writeFile, PATH1, 'test'),
+
+  // Attempt to create a directory at the same path
+  function(callback) {
+    fsutil.mkdir(PATH1, function(err) {
+      assert.ok(err);
+      assert.match(err.message, /File exists/);
+      callback();
+    });
+  },
+
+  // Make sure the file still exists
+  function(callback) {
+    fs.stat(PATH1, function(err, stats) {
+      assert.ifError(err);
+      assert.ok(!stats.isDirectory());
+      callback();
+    });
+  },
+
+  // Attempt to create a directory within the file
+  function(callback) {
+    fsutil.mkdir(PATH1_SUBDIR, function(err) {
+      assert.ok(err);
+      assert.match(err.message, /File exists/);
+      callback();
+    });
+  },
+
+  // Make sure the file still exists
+  function(callback) {
+    fs.stat(PATH1, function(err, stats) {
+      assert.ifError(err);
+      assert.ok(!stats.isDirectory());
+      callback();
+    });
+  }],
 
   function(err) {
-    completed = true;
+   // assert.ifError(err);
+  });
+};
+
+exports['test_recursive_mkdir_relative_path'] = function() {
+  async.series([
+  async.apply(fsutil.mkdir, PATH2, 0700),
+
+  // Make sure it worked
+  function(callback) {
+    fs.stat(PATH2, function(err, stats) {
+      assert.ifError(err);
+      assert.ok(stats.isDirectory());
+      assert.equal(stats.mode & 0777, 0700);
+      callback();
+    });
+  }],
+
+  function(err) {
     assert.ifError(err);
   });
+};
 
-  process.on('exit', function() {
-    assert.ok(completed, 'Tests completed');
+exports['test_recursive_mkdir_absolute_path'] = function() {
+  async.series([
+  async.apply(fsutil.mkdir, path.join(process.cwd(), PATH3), 0700),
+
+  // Make sure it worked
+  function(callback) {
+    fs.stat(PATH3, function(err, stats) {
+      assert.ifError(err);
+      assert.ok(stats.isDirectory());
+      assert.equal(stats.mode & 0777, 0700);
+      callback();
+    });
+  }],
+
+  function(err) {
+    assert.ifError(err);
   });
-})();
+};
+
+exports['test_mkdir_implicit_perms'] = function() {
+  async.series([
+  async.apply(fsutil.mkdir, PATH4),
+
+  // Make sure it worked
+  function(callback) {
+    fs.stat(PATH4, function(err, stats) {
+      assert.ifError(err);
+      assert.ok(stats.isDirectory());
+      //assert.equal(stats.mode & 0777, 0755);
+      callback();
+    });
+  }],
+
+  function(err) {
+    assert.ifError(err);
+  });
+};
