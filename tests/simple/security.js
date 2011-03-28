dd63561dfd47495e9761e2f289b50971d901d46c
Russell Haering <russell@cloudkick.com>
2011-03-28 11:04:40 -0700

    add more certgen tests

--- tests/simple/security.js
+++ tests/simple/security.js
@@ -24,27 +24,43 @@ var async = require('extern/async');
 var misc = require('util/misc');
 var assert = require('./../assert');
 
+exports['setUp'] = function(callback) {
+  fs.mkdir('.tests/certs', 0700, callback);
+};
+
 exports['test_openssl_cert_generation'] = function() {
-require('util').debug('in')
-  async.series([
-    async.apply(fs.mkdir, '.tests/certs', 0700),
-
-    function(callback) {
-      var hostname = 'testhostnamerare' + misc.randstr(5);
-      var keypath = '.tests/certs/t.key';
-      var crtpath = '.tests/certs/t.crt';
-      certgen.genSelfSigned(hostname, keypath, crtpath, function(err) {
-        assert.ifError(err);
-        exec('openssl x509 -noout -subject -in .tests/certs/t.crt', function(err, stdout, stderr) {
-          assert.ifError(err);
-          assert.equal('subject= /CN=' + hostname, misc.trim(stdout));
-          callback();
-        });
-      });
-    }
-  ],
-
-  function(err) {
+  var hostname = 'testhostnamerare' + misc.randstr(5);
+  var keypath = '.tests/certs/t.key';
+  var crtpath = '.tests/certs/t.crt';
+  certgen.genSelfSigned(hostname, keypath, crtpath, function(err) {
     assert.ifError(err);
+    exec('openssl x509 -noout -subject -in .tests/certs/t.crt', function(err, stdout, stderr) {
+      assert.ifError(err);
+      assert.equal('subject= /CN=' + hostname, misc.trim(stdout));
+    });
+  });
+};
+
+exports['test_openssl_key_generation'] = function() {
+  var keypath = '.tests/certs/t2.key';
+  certgen.genKey(keypath, function(err) {
+    assert.ifError(err);
+    fs.readFile(keypath, 'utf8', function(err, text) {
+      assert.ifError(err);
+      assert.match(text, /BEGIN RSA PRIVATE KEY/);
+      assert.match(text, /END RSA PRIVATE KEY/);
+    })
+  });
+};
+
+exports['test_openssl_csr_generation'] = function() {
+  var keypath = '.tests/certs/t3.key';
+  certgen.genKey(keypath, function(err) {
+    assert.ifError(err);
+    certgen.getCSR('foo.example.com', keypath, function(err, csr) {
+      assert.ifError(err);
+      assert.match(csr, /BEGIN CERTIFICATE REQUEST/);
+      assert.match(csr, /END CERTIFICATE REQUEST/);
+    });
   });
 };
