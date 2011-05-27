a2184994273d79fb72bfddff4ae01382a1c58aac
Tomaz Muraus <tomaz@cloudkick.com>
2011-05-28 00:25:48 +0200

    Add tests for adding a new ca.

--- tests/simple/http-endpoints-ca.js
+++ tests/simple/http-endpoints-ca.js
@@ -15,16 +15,35 @@
  * limitations under the License.
  */
 
+var sprintf = require('sprintf').sprintf;
 var async = require('async');
 
 var http = require('services/http');
+var dotfiles = require('util/client_dotfiles');
 var testUtil = require('util/test');
 var testConstants = require('./../constants');
 
 var getServer = http.getAndConfigureServer;
+var DEFAULT_REMOTE = testConstants.AVAILABLE_REMOTES['localhost_http1'];
 
 exports['test_ca_http_endpoint'] = function(test, assert) {
-  async.series([
+  var remote = {
+      'url': sprintf('%s://%s:%s', DEFAULT_REMOTE.protocol, DEFAULT_REMOTE.ip,
+                                   DEFAULT_REMOTE.port),
+      'hostname': DEFAULT_REMOTE.hostname,
+      'port': DEFAULT_REMOTE.port,
+      'name': 'test-ca',
+      'fingerprint': 'abcd',
+      'is_default': true,
+      'global': true
+    };
+
+    var certOpts = {
+      'hostname': 'localhost',
+      'email': 'test@localhost.dev'
+    };
+
+  async.waterfall([
     function testListRequestsIsEmpty(callback) {
       var req = testUtil.getReqObject('/ca', 'GET', testConstants.API_VERSION);
 
@@ -34,7 +53,54 @@ exports['test_ca_http_endpoint'] = function(test, assert) {
         assert.deepEqual(res.body, []);
         callback();
       });
-    }
+    },
+
+    function testAddRequestInvalidHostname(callback) {
+      var req = testUtil.getReqObject('/ca/localhost./', 'PUT', testConstants.API_VERSION);
+
+      assert.responseJson(getServer(), req, function(res) {
+        // At the beginning there should be no requests
+        assert.equal(res.statusCode, 400);
+        assert.match(res.body.message, /invalid hostname/i);
+        callback();
+      });
+    },
+
+    function testAddRequestInvalidCSR(callback) {
+      var req = testUtil.getReqObject('/ca/localhost.test/', 'PUT', testConstants.API_VERSION);
+      req.body = 'invalidcsr';
+
+      assert.responseJson(getServer(), req, function(res) {
+        assert.equal(res.statusCode, 500);
+        assert.match(res.body.message, /invalid csr/i);
+        callback();
+      });
+    },
+
+    function createTestCSR(callback) {
+      function getherCertOpts(callback) {
+        callback(null, certOpts);
+      }
+
+      dotfiles.ensureRemoteCSR(remote, getherCertOpts, callback);
+    },
+
+    function onCertGenerated(callback) {
+      // Read the csr file
+      dotfiles.loadRemoteCSR(remote, callback);
+    },
+
+    function testAddRequestValidCSR(csrBuf, callback) {
+      var req = testUtil.getReqObject('/ca/localhost.test/', 'PUT', testConstants.API_VERSION);
+      req.body = csrBuf.toString('utf8');
+
+      assert.responseJson(getServer(), req, function(res) {
+        assert.equal(res.statusCode, 202);
+        assert.match(res.body.status, /awaiting approval/i);
+        callback();
+      });
+    },
+
   ],
 
   function(err) {
