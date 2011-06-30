e2bc251d4ea5fb2065528940f0a7b4e2bf1d2c23
Tomaz Muraus <tomaz@cloudkick.com>
2011-06-30 19:21:19 +0200

    Add some more tests and make sure config is reloaded before running every test.

--- tests/simple/test-plugin-manager.js
+++ tests/simple/test-plugin-manager.js
@@ -17,91 +17,132 @@
 
 var path = require('path');
 
+var async = require('async');
+
 var config = require('util/config');
 var manager = require('plugins').manager;
 
-exports['setUp'] = function(test, assert) {
+function loadConfig(callback) {
   config.configFiles = [
     path.join(__dirname, '../test.conf')
   ];
 
-  config.setupAgent(test.finish);
-};
+  config.setupAgent(callback);
+}
+
+/*
+ * Utility function which loads the config before running the test.
+ */
+function runTest(testFunction, test) {
+  async.series([
+    async.apply(loadConfig),
+    async.apply(testFunction)
+  ],
+
+  function(err) {
+    test.finish();
+  });
+}
 
 exports['test_getAvailablePlugins_success'] = function(test, assert) {
   var pluginManager = new manager.PluginManager();
 
-  assert.deepEqual(pluginManager._availablePlugins, {});
-  pluginManager.getAvailablePlugins(function(err, availablePlugins) {
-    assert.ifError(err);
+  runTest(function test(callback) {
+    assert.deepEqual(pluginManager._availablePlugins, {});
+    pluginManager.getAvailablePlugins(function(err, availablePlugins) {
+      assert.ifError(err);
 
-    assert.ok(Object.keys(availablePlugins).length === 1);
-    assert.ok(Object.keys(pluginManager._availablePlugins).length === 1);
-    assert.ok(availablePlugins.hasOwnProperty('cast-github'));
-    assert.ok(pluginManager._availablePlugins.hasOwnProperty('cast-github'));
-    test.finish();
-  });
+      assert.ok(Object.keys(availablePlugins).length === 1);
+      assert.ok(Object.keys(pluginManager._availablePlugins).length === 1);
+      assert.ok(availablePlugins.hasOwnProperty('cast-github'));
+      assert.ok(pluginManager._availablePlugins.hasOwnProperty('cast-github'));
+      callback();
+    });
+  }, test);
 };
 
 exports['test_getAvailablePlugins_inexistent_directory'] = function(test,
                                                                     assert) {
   var pluginManager = new manager.PluginManager();
-  config.currentConfig['plugins']['root'] = '/some/nonexistent/directory';
 
-  assert.deepEqual(pluginManager._availablePlugins, {});
-  pluginManager.getAvailablePlugins(function(err, availablePlugins) {
-    assert.ifError(err);
+  runTest(function test(callback) {
+    config.currentConfig['plugins']['root'] = '/some/nonexistent/directory';
 
-    assert.ok(Object.keys(availablePlugins).length === 0);
     assert.deepEqual(pluginManager._availablePlugins, {});
-    test.finish();
-  });
+    pluginManager.getAvailablePlugins(function(err, availablePlugins) {
+      assert.ifError(err);
+
+      assert.ok(Object.keys(availablePlugins).length === 0);
+      assert.deepEqual(pluginManager._availablePlugins, {});
+      callback();
+    });
+  }, test);
 };
 
 exports['test_getEnabledPlugins_one_enabled_plugin'] = function(test, assert) {
   var pluginManager = new manager.PluginManager();
 
-  pluginManager.getEnabledPlugins(function(err, enabledPlugins) {
-    assert.ifError(err);
+  runTest(function test(callback) {
+    pluginManager.getEnabledPlugins(function(err, enabledPlugins) {
+      assert.ifError(err);
 
-    assert.ok(Object.keys(enabledPlugins).length === 1);
-    assert.ok(enabledPlugins.hasOwnProperty('cast-github'));
-    assert.deepEqual(enabledPlugins, { 'cast-github': { 'foo': 'bar' }});
-    test.finish();
-  });
+      assert.ok(Object.keys(enabledPlugins).length === 1);
+      assert.ok(enabledPlugins.hasOwnProperty('cast-github'));
+      assert.deepEqual(enabledPlugins, { 'cast-github': { 'foo': 'bar' }});
+      callback();
+    });
+  }, test);
 };
 
 exports['test_getEnabledPlugins_no_enabled_plugins'] = function(test, assert) {
   var pluginManager = new manager.PluginManager();
-  config.currentConfig['plugins']['enabled'] = {};
 
-  pluginManager.getEnabledPlugins(function(err, enabledPlugins) {
-    assert.ifError(err);
+  runTest(function test(callback) {
+    config.currentConfig['plugins']['enabled'] = {};
 
-    assert.ok(Object.keys(enabledPlugins).length === 0);
-    test.finish();
-  });
+    pluginManager.getEnabledPlugins(function(err, enabledPlugins) {
+      assert.ifError(err);
+
+      assert.ok(Object.keys(enabledPlugins).length === 0);
+      callback();
+    });
+  }, test);
 };
 
 exports['test_getPluginManifest_plugin_does_not_exist'] = function(test, assert) {
   var pluginManager = new manager.PluginManager();
 
-  pluginManager.getPluginManifest('inexistent-plugin', function(err,
-                                                                pluginManifest) {
-    assert.ok(err);
-    assert.match(err.message, /does not exist/);
-    test.finish();
-  });
+  runTest(function test(callback) {
+    pluginManager.getPluginManifest('inexistent-plugin', function(err,
+                                                                  pluginManifest) {
+      assert.ok(err);
+      assert.match(err.message, /does not exist/);
+      callback();
+    });
+  }, test);
 };
 
-/*exports['test_getPluginSettings_success'] = function(test, assert) {
+exports['test_getPluginSettings_success'] = function(test, assert) {
   var pluginManager = new manager.PluginManager();
 
-  pluginManager.getPluginSettings('cast-github', function(err, pluginSettings) {
-    assert.ifError(err);
-    assert.ok(!err);
+  runTest(function test(callback) {
+    pluginManager.getPluginSettings('cast-github', function(err, pluginSettings) {
+      assert.ok(!err);
+      assert.deepEqual(pluginSettings, { 'foo': 'bar' });
+      callback();
+    });
+  }, test);
+};
 
-    assert.deepEqual(pluginSettings, { 'foo': 'bar' });
-    test.finish();
-  });
-};*/
+exports['test_getPluginSettings_not_enabled'] = function(test, assert) {
+  var pluginManager = new manager.PluginManager();
+
+  runTest(function test(callback) {
+    pluginManager.getPluginSettings('some-not-enabled-plugins', function(err,
+                                                                pluginSettings) {
+      assert.ok(err);
+      assert.match(err.message, /not enabled/i);
+      callback();
+    });
+  }, test);
+};
